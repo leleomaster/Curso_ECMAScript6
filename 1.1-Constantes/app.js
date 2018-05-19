0a1ef479b8c8652bb51ba1d89e2c6850b2fd2a10
leonardo <ljcjleonardo@gmail.com>
2018-05-19 18:41:46 -0300

    crinado constantes no ECMAScript6

---
+++ 1.1-Constantes/app.js
@@ -0,0 +1,9 @@
+const TAMANHO = 100;
+const IGV = 0.18;
+
+console.log(TAMANHO);
+alert(IGV);
+
+TAMANHO = 10;// Erro, pois não podemos alterar uma variável constante!
+
+alert(TAMANHO); // Não será exibido o alertt por causa do erro a cima.
