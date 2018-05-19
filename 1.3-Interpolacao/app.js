a3dadebae8f48fcae304f6fd0ed48f8224b61c4e
leonardo <ljcjleonardo@gmail.com>
2018-05-19 19:42:14 -0300

    Criando interpolação no ECMA6

---
+++ 1.3-Interpolacao/app.js
@@ -0,0 +1,17 @@
+﻿const POKEMON = {
+    nome: "Pikachu",
+    cor: "amarelo",
+    evolucao:"Raichu"
+};
+
+// Com interpolação
+let descricao = `Meu nome é  ${POKEMON.nome}, eu sou da cor ${POKEMON.cor} , minha evolução é ${POKEMON.evolucao}`
+
+// Sem interpolação
+/*
+    var descricao = "Meu nome é "+ POKEMON.nome+"
+                    , eu sou da cor "+POKEMON.cor+"
+                    , minha evolução é "+POKEMON.evolucao
+*/
+
+console.log(descricao);
