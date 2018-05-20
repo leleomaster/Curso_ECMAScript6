bd1fde7964e4926d978a3d5a59fd7afad2a3b076
leonardo <ljcjleonardo@gmail.com>
2018-05-20 15:56:57 -0300

    Crinado Métodos nativos String no ECMAScript6

---
+++ 21-MetodosNativos/21.3-String/app.js
@@ -0,0 +1,21 @@
+﻿let mensagem = "Olá mundo";
+
+console.log(mensagem.repeat(3));
+
+console.log(mensagem.startsWith("Olá"));
+
+console.log(mensagem.startsWith("Olá",2));
+
+console.log(mensagem.startsWith("Olá",10));
+
+console.log(mensagem.endsWith("mundo"));
+
+console.log(mensagem.endsWith("Olá",3));
+
+console.log(mensagem.endsWith("lá",3)); // 3 tamanho da string
+
+console.log(mensagem.includes("mundo"));
+
+console.log(mensagem.includes("mundos"));
+
+console.log(mensagem.includes("mundo",4));// 4 = a partir do número 4 verifique se tem mundo
