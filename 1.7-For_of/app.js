338a3ab17c643d0d1e68ce47a30bb85930f719bb
leonardo <ljcjleonardo@gmail.com>
2018-05-19 22:30:02 -0300

    Crinado FORs no ECMAScript6

---
+++ 1.7-For_of/app.js
@@ -0,0 +1,11 @@
+﻿let linguagens = ["c#", "php", "ruby","python"]
+
+let linguagem = "Não será imprimida no FOR"
+
+for (let linguagem of linguagens) {
+    console.log(linguagem);
+}
+
+console.log("Após a execução do FOR => linguagens")
+
+console.log(linguagem);
